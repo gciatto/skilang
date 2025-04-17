14501dcda74c3550a96b6e0b0ad7a7f501976ec8
Mattia Matteini <matteinimattia@gmail.com>
2025-04-17 16:49:30 +0200

    chore: add static-checks poe command

--- tests/features/steps/parsing.py
+++ tests/features/steps/parsing.py
@@ -1,4 +1,5 @@
 from behave import use_step_matcher, given, when
+
 from skilang import parse, Formula
 
 use_step_matcher("parse")
@@ -21,4 +22,17 @@ def step_expression_parsed(context):
     if "exp" not in context:
         raise ValueError("Expression not provided.")
     parsed: Formula = parse(context.exp)
-    context.parsed_exp = parsed
+    context.parsed_exp = context.result = parsed
+
+
+@when("the expression is evaluated")
+def step_expression_evaluated(context):
+    """
+    :type context: behave.runner.Context
+    """
+    if "parsed_exp" not in context:
+        raise ValueError("Parsed expression not provided.")
+    if "assignments" not in context:
+        raise ValueError("Assignments not provided.")
+    parsed: Formula = context.parsed_exp
+    context.result = parsed.evaluate(**context.assignments)
