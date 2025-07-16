d13b4804c54ac37e8354d9f0c069bb8db45353f8
Mattia Matteini <matteinimattia@gmail.com>
2025-07-16 11:49:39 +0200

    chore(deps): update torchic dependency

--- skilang/__init__.py
+++ skilang/__init__.py
@@ -1,4 +1,4 @@
-from typing import Dict, Iterable, Callable
+from typing import Dict, Iterable
 
 from torch import Tensor
 
--- skilang/testimages/images.py
+++ skilang/testimages/images.py
@@ -12,20 +12,22 @@ from tests.resources.datasets import dataset_path
 
 
 def load_images(filename):
-    with open(filename, 'rb') as f:
-        magic, num, rows, cols = struct.unpack('>IIII', f.read(16))
+    with open(filename, "rb") as f:
+        magic, num, rows, cols = struct.unpack(">IIII", f.read(16))
         data = np.frombuffer(f.read(), dtype=np.uint8)
         return torch.tensor(data, dtype=torch.uint8).view(num, 28, 28)
 
+
 def load_labels(filename):
-    with open(filename, 'rb') as f:
-        magic, num = struct.unpack('>II', f.read(8))
+    with open(filename, "rb") as f:
+        magic, num = struct.unpack(">II", f.read(8))
         data = np.frombuffer(f.read(), dtype=np.uint8)
         return torch.tensor(data, dtype=torch.long)
 
+
 def preprocess_images(images):
     images = images.float() / 255.0  # Normalize to [0, 1]
-    return images.unsqueeze(1)       # Add channel dimension: [N, 1, 28, 28]
+    return images.unsqueeze(1)  # Add channel dimension: [N, 1, 28, 28]
 
 
 def create_sum_mnist_pairs(images, labels, num_pairs):
@@ -50,22 +52,21 @@ def create_sum_mnist_pairs(images, labels, num_pairs):
     return torch.stack(pairs), torch.tensor(sums)
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     # Load the dataset
-    train_images = load_images(dataset_path('archive/train-images.idx3-ubyte'))
-    train_labels = load_labels(dataset_path('archive/train-labels.idx1-ubyte'))
+    train_images = load_images(dataset_path("archive/train-images.idx3-ubyte"))
+    train_labels = load_labels(dataset_path("archive/train-labels.idx1-ubyte"))
     train_images = preprocess_images(train_images)
 
-    test_images = load_images(dataset_path('archive/t10k-images.idx3-ubyte'))
-    test_labels = load_labels(dataset_path('archive/t10k-labels.idx1-ubyte'))
+    test_images = load_images(dataset_path("archive/t10k-images.idx3-ubyte"))
+    test_labels = load_labels(dataset_path("archive/t10k-labels.idx1-ubyte"))
     test_images = preprocess_images(test_images)
 
     print(train_images.shape)
     print(train_labels.shape)
 
     image = train_images[1]
-    plt.imshow(image.squeeze(), cmap='gray')
+    plt.imshow(image.squeeze(), cmap="gray")
     plt.show()
 
     ### Create data loaders
