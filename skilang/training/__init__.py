fix: injectio loss computation

skilang/training/__init__.py
@@ -114,6 +114,7 @@ def create_regularization_fn(
         }
 
         regularization_tensor: Tensor = torch.zeros(input_batch.shape[0]).to(input_batch.device)
+        regularization_scalar: Tensor = torch.tensor(0.0).to(input_batch.device)
 
         def create_model_output_fn(pred_snapshot):
             return lambda batch_snapshot: pred_snapshot.argmax(dim=1)
@@ -131,8 +132,14 @@ def create_regularization_fn(
 
         for index, constraint in enumerate(constraints):
             respected_constraint = constraint.clause.evaluate(**assignments)
+
             if respected_constraint.dim() == 0:
-                return torch.tensor([0], device='mps') if respected_constraint.item() else torch.tensor([10], device='mps')
+                regularization_scalar += constraint.weight * respected_constraint
+
+            elif respected_constraint.dim() == 1:
+                d = respected_constraint.shape[0]
+                regularization_scalar += constraint.weight * (d - respected_constraint.sum()) / d
+
             else:
                 respected_condition: Tensor = torch.tensor([])
 
@@ -162,6 +169,7 @@ def create_regularization_fn(
 
                 multiplier: float = 1
                 regularization_tensor[apply_penalty] += multiplier * constraint.weight
-        return regularization_tensor
+
+        return regularization_tensor if torch.zeros_like(regularization_scalar) else regularization_scalar
 
     return regularization_fn

skilang/training/skitrainer.py
@@ -8,33 +8,40 @@ from torchic.nn.trainers import AbstractTrainer
 
 class SkiTrainer(AbstractTrainer):
     def __init__(
-        self, model: NeuralNetwork, regularization_fn: Callable[[Tensor, Tensor, Tensor], Tensor]
+            self, model: NeuralNetwork, regularization_fn: Callable[[Tensor, Tensor, Tensor], Tensor]
     ) -> None:
         super().__init__(model)
         self.regularization_fn: Callable[[Tensor, Tensor, Tensor], Tensor] = regularization_fn
 
     def train_step(
-        self, input_batch: Tensor, target: Tensor, loss_fn: Callable
+            self, input_batch: Tensor, target: Tensor, loss_fn: Callable
     ) -> Tuple[Tensor, torch.types.Number]:
         # Compute prediction error
         pred: Tensor = self.model(input_batch)
         loss: Tensor = loss_fn(pred, target)
         regularization_tensor: Tensor = self.regularization_fn(input_batch, pred, target)
-        modified_loss = loss + regularization_tensor
+        if loss.dim() == regularization_tensor.dim():
+            modified_loss = loss + regularization_tensor
+        else:
+            modified_loss = loss.mean() + regularization_tensor
         # Backpropagation
         # if loss is not reduced to a scalar
         if modified_loss.dim() != 0:
-            modified_loss = modified_loss
+            modified_loss = modified_loss.mean()
 
         modified_loss.backward()
         return pred, modified_loss.item()
 
     def eval_step(
-        self, input_batch: Tensor, target: Tensor, loss_fn: Callable
+            self, input_batch: Tensor, target: Tensor, loss_fn: Callable
     ) -> Tuple[Tensor, torch.types.Number]:
         pred: Tensor = self.model(input_batch)
         loss: Tensor = loss_fn(pred, target)
-        modified_loss = loss + self.regularization_fn(input_batch, pred, target)
+        regularization_tensor = self.regularization_fn(input_batch, pred, target)
+        if loss.dim() == regularization_tensor.dim():
+            modified_loss = loss + regularization_tensor
+        else:
+            modified_loss = loss.mean() + regularization_tensor
         if modified_loss.dim() != 0:
-            modified_loss = modified_loss
+            modified_loss = modified_loss.mean()
         return pred, modified_loss.item()